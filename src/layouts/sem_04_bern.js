a9e1bb32d29bc7e76d9ac666009b718409dbb017
Stackbit Code Editor <projects@stackbit.com>
2021-03-29 23:25:55 +0000

    sem_04_bern.js: changed

--- src/layouts/sem_04_bern.js
+++ src/layouts/sem_04_bern.js
@@ -27,9 +27,7 @@ export default class Project extends React.Component {
                   {markdownify(_.get(this.props, 'page.markdown', null))}
                 </div>
               </article>
-</header>
-                {_.get(this.props, 'page.frontmatter.image', null) && (
-                <div className="post-image">
+
             </div>
             </Layout>
         );
